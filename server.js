cca67be3256abc707194e299d485ebe6d0c0e165
vijay-kartik <kartik.iit96@gmail.com>
2023-10-25 02:28:10 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const http = require('http');
-const socket = require('websocket').server
+const http = require("http");
+const Socket = require("websocket").server;
 const server = http.createServer(() => {});
 
 server.listen(3000, () => {
     console.log("Server started listening on port 3000");
-})
+});
 
-const websocket = new socket({httpServer:server});
+const websocket = new Socket({httpServer:server});
 
 websocket.on('request', (req) => {
     const connection = req.accept();
     console.log(connection);
-})
-
-websocket.on('create or join', function(room) {
-    console.log('Received request to create or join room' + room);
-    
 });
+
+// websocket.on('create or join', function(room) {
+//     console.log('Received request to create or join room' + room);
+    
+// });
